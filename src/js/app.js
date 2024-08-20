9be50688e407f43fc305de2a3f0d1ea158099dde
Candela <rabbiacandela@gmail.com>
2024-08-20 17:10:40 -0300

    primer commit

---
+++ src/js/app.js
@@ -0,0 +1,77 @@
+document.addEventListener('DOMContentLoaded', function(){
+    crearGaleria();
+});
+
+function crearGaleria(){
+    const galeria = document.querySelector('.galeria-imagenes');
+
+    for(let i=1 ; i<=12; i++){
+        const imagen = document.createElement('IMG');
+        imagen.src = `src/img/thumb/${i}.jpg`;
+        imagen.dataset.imagenId = i;
+        //a;adir la funcion mostrat imagen
+        imagen.onclick = mostrarFoto;
+
+        const lista = document.createElement('LI');
+        lista.appendChild(imagen);
+
+        galeria.appendChild(lista);
+
+        
+    }
+
+}
+
+function mostrarFoto(e){
+    const id = parseInt(e.target.dataset.imagenId);
+
+    //genera imagen
+    const imagen = document.createElement('IMG');
+    imagen.src = `src/img/grande/${id}.jpg`;
+
+    const overlay = document.createElement('div');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+
+    //cerrar imagen con click
+    overlay.onclick = function(){
+        overlay.remove();
+    }
+
+    //boton para cerrar imagen
+    const cerrarImagen = document.createElement('p');
+    cerrarImagen.textContent = 'X';
+    cerrarImagen.classList.add('btn-cerrar');
+
+    //cerrar imagen
+    cerrarImagen.onclick = function(){
+        overlay.remove();
+    }
+
+
+    overlay.appendChild(cerrarImagen);
+
+    //mostrar en html
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fijar-body');
+
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    scrollNav();
+});
+
+function scrollNav(){
+    const enlace = document.querySelectorAll('.navegacion-principal a');
+
+    enlace.forEach( function(enlaces){
+        enlaces.addEventListener('click', function(e){
+            e.preventDefault();
+            const seccion= document.querySelector(e.target.attributes.href.value);
+            seccion.scrollIntoView({
+                behavior: 'smooth',
+            });
+        });
+    });
+}
